44cacb76577145e7660ac15e220371668f307e58
kia4545 <81552960+kia4545@users.noreply.github.com>
2022-10-05 23:36:50 +0900

    ファイルを共通化するためにリファクタリング

---
+++ app/template/error.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1); ?>
+
+<?php require_once(dirname(__DIR__) . "/template/header.php"); ?>
+
+<div class="clearfix">
+    <?php require_once(dirname(__DIR__) . "/template/menu.php"); ?>
+    <div id="main">
+        <div class="error_message"><?php echo $errorMessage; ?></div>
+    </div>
+</div>
+<?php require_once(dirname(__DIR__) . "/template/footer.php"); ?>
